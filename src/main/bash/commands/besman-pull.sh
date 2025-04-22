4b115398874bf18af49eb0211c8fda8b56a7607d
asa1997 <arunsureshampadath@gmail.com>
2025-04-22 22:19:53 +0530

    added pulling of steps file

--- src/main/bash/commands/besman-pull.sh
+++ src/main/bash/commands/besman-pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 function __bes_pull {
     local playbook_name="$1"
     local playbook_version="$2"
@@ -17,42 +17,45 @@ function __bes_pull {
     local fetch_result=$?
 
     case $fetch_result in
-        0)
-            __besman_echo_green "Playbook $playbook_name $playbook_version added successfully."
-            ;;
-        1)
-            __besman_echo_red "Failed to fetch playbook $playbook_name $playbook_version."
-            ;;
-        2)
-            __besman_echo_yellow "Playbook $playbook_name $playbook_version already exists."
-            read -rp "Do you wish to overwrite it? (y/n): " overwrite
-            if [[ "$overwrite" == "y" ]]; then
-                rm "$BESMAN_PLAYBOOK_DIR/besman-$playbook_name-$playbook_version-playbook.sh" || {
-                    __besman_echo_red "Failed to overwrite existing playbook."
-                    return 1
-                }
-                __besman_fetch_playbook "$playbook_name" "$playbook_version"
-                fetch_result=$?
-                [[ $fetch_result -eq 0 ]] && __besman_echo_green "Playbook $playbook_name $playbook_version overwritten successfully."
-            elif [[ "$overwrite" == "n" ]]; then
-                __besman_echo_white "Skipping playbook overwrite."
-            else
-                __besman_echo_red "Invalid option. Skipping playbook overwrite."
-            fi
-            ;;
-        *)
-            __besman_echo_red "Unknown error occurred."
-            ;;
+    0)
+        __besman_fetch_steps_file "$playbook_name" "$playbook_version" || {
+            __besman_echo_red "Failed to fetch steps file for playbook $playbook_name $playbook_version."
+            return 1
+        }
+        __besman_echo_green "Playbook $playbook_name $playbook_version added successfully."
+        ;;
+    1)
+        __besman_echo_red "Failed to fetch playbook $playbook_name $playbook_version."
+        ;;
+    2)
+        __besman_echo_yellow "Playbook $playbook_name $playbook_version already exists."
+        read -rp "Do you wish to overwrite it? (y/n): " overwrite
+        if [[ "$overwrite" == "y" ]]; then
+            rm "$BESMAN_PLAYBOOK_DIR/besman-$playbook_name-playbook-$playbook_version.sh" || {
+                __besman_echo_red "Failed to overwrite existing playbook."
+                return 1
+            }
+            __besman_fetch_playbook "$playbook_name" "$playbook_version"
+            fetch_result=$?
+            [[ $fetch_result -eq 0 ]] && __besman_echo_green "Playbook $playbook_name $playbook_version overwritten successfully."
+        elif [[ "$overwrite" == "n" ]]; then
+            __besman_echo_white "Skipping playbook overwrite."
+        else
+            __besman_echo_red "Invalid option. Skipping playbook overwrite."
+        fi
+        ;;
+    *)
+        __besman_echo_red "Unknown error occurred."
+        ;;
     esac
 
-    return $fetch_result
 }
- 
+
 function __besman_fetch_playbook {
     local playbook_name="$1"
     local playbook_version="$2"
-    local playbook_file="$BESMAN_PLAYBOOK_DIR/besman-$playbook_name-$playbook_version-playbook.sh"
-    local playbook_url="https://raw.githubusercontent.com/$BESMAN_PLAYBOOK_REPO/$BESMAN_PLAYBOOK_REPO_BRANCH/playbooks/besman-$playbook_name-$playbook_version-playbook.sh"
+    local playbook_file="$BESMAN_PLAYBOOK_DIR/besman-$playbook_name-playbook-$playbook_version.sh"
+    local playbook_url="https://raw.githubusercontent.com/$BESMAN_PLAYBOOK_REPO/$BESMAN_PLAYBOOK_REPO_BRANCH/playbooks/besman-$playbook_name-playbook-$playbook_version.sh"
 
     if [[ -f "$playbook_file" ]]; then
         return 2
@@ -60,9 +63,27 @@ function __besman_fetch_playbook {
 
     __besman_check_url_valid "$playbook_url" || return 1
 
-    if __besman_secure_curl "$playbook_url" >> "$playbook_file"; then
+    if __besman_secure_curl "$playbook_url" >>"$playbook_file"; then
         return 0
     else
         return 1
+    fi
+}
+
+function __besman_fetch_steps_file() {
+    local playbook_name="$1"
+    local playbook_version="$2"
+    local API="https://api.github.com/repos/$BESMAN_PLAYBOOK_REPO/contents/playbooks?ref=$BESMAN_PLAYBOOK_REPO_BRANCH"
+    local steps_file_base_name="besman-$playbook_name-playbook-$playbook_version-steps"
+    local download_url
+    # to get the extension of the file
+    download_url=$(curl -s "$API" | jq -r --arg name "$steps_file_base_name" '.[] | select(.name | test("^" + $name + "\\.(sh|md|ipynb)$")) | .download_url')
+
+    if [[ -n "$download_url" ]]; then
+        __besman_echo_white "Downloading steps file from $download_url"
+        curl -L "$download_url" -o "${download_url##*/}"
+    else
+        echo "No matching file found in GitHub API."
+        exit 1
     fi
 }
