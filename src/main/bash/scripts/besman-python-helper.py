f85c16dee76b4e54089321fc888858dd1b9ad724
asa1997 <arun.suresh@wipro.com>
2025-04-29 14:52:32 +0530

    updated

--- src/main/bash/scripts/besman-python-helper.py
+++ src/main/bash/scripts/besman-python-helper.py
@@ -4,4 +4,13 @@ def construct_raw_url(namespace, repo, branch):
     repo = repo
     branch = branch
     platform = os.environ.get("BESMAN_CODE_COLLAB_PLATFORM")
-    ca
+    # https://raw.githubusercontent.com/{env_repo}/{branch}/environment-metadata.json
+    # http://lab.o31e.com/{env_repo}/-/raw/{branch}/environment-metadata.json
+    if platform == "github":
+        url = f'https://raw.githubusercontent.com/{namespace}/{repo}/{branch}'
+    elif platform == "gitlab":
+        platform_url = os.environ.get("BESMAN_CODE_COLLAB_URL")
+        url = f'{platform_url}/{namespace}/{repo}/-/raw/{branch}'
+    else:
+        print(f"Error: Unsupported platform: {platform}")
+    return url
